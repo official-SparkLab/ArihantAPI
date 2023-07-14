370c9ee5783684cd51e61b27a7e2946dc021e9f0
Prathamesh Varale <prathameshvarale07192@gmail.com>
2023-07-14 12:44:23 +0530

    Update order product and add product work done

--- app/Http/Controllers/Ordered_Product_Controller.php
+++ app/Http/Controllers/Ordered_Product_Controller.php
@@ -61,27 +61,44 @@ class Ordered_Product_Controller extends Controller
 
 
 
-    public function updateOrder(Request $request, $unique_id)
-    {
-        $product = Ordered_Product_Model::where('unique_id', $unique_id)->first();
-    
-        if (!$product) {
-            // Product not found, create a new one
-            $product = new Ordered_Product_Model();
-            $product->unique_id = $unique_id;
-        }
-    
-        $product->p_id = $request->input('p_id');
+ public function updateOrder(Request $request, $unique_id)
+{
+    $product = Ordered_Product_Model::where('unique_id', $unique_id)->first();
+
+    if (!$product) {
+        // Product not found, create a new one
+        $product = new Ordered_Product_Model();
+        $product->unique_id = $unique_id;
+    }
+
+    // Check if the product ID has changed
+    $inputProductId = $request->input('p_id');
+    if ($product->p_id !== $inputProductId) {
+        // Product ID has changed, create a new record
+        $newProduct = new Ordered_Product_Model();
+        $newProduct->unique_id = $unique_id;
+        $newProduct->p_id = $inputProductId;
+        $newProduct->product_name = $request->input('product_name');
+        $newProduct->quantity = $request->input('quantity');
+        $newProduct->rate = $request->input('rate');
+        $newProduct->total = $request->input('total');
+        $newProduct->save();
+
+        $message = 'New product added successfully';
+    } else {
+        // Product ID is the same, update the existing record
         $product->product_name = $request->input('product_name');
         $product->quantity = $request->input('quantity');
         $product->rate = $request->input('rate');
         $product->total = $request->input('total');
-    
         $product->save();
-    
-        $message = $product->wasRecentlyCreated ? 'Product added successfully' : 'Product updated successfully';
-        return response()->json(['Message' => $message]);
+
+        $message = 'Product updated successfully';
     }
+
+    return response()->json(['Message' => $message]);
+}
+
     
     
 
