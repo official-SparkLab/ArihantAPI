5916f1fbd5ff2c24dc84a21df21916a95257384a
Prathamesh Varale <prathameshvarale07192@gmail.com>
2023-08-08 11:01:32 +0530

    Sale payble and order details table joined

--- routes/api.php
+++ routes/api.php
@@ -236,6 +236,9 @@ Route::get('/fetchBarcode/{unique_id}',[Barcode_Controller::class,'fetchBarcode'
 
 Route::get('/fetchWeight/{unique_id}',[Weight_Controller::class,'fetchWeight']);
 
+Route::get('/fetchOrderandSale/{contact_no}',[JoinTableController::class,'fetchCombinedData']);
+
+
 //User Routing 
 
 Route::post('/addUser',[UserController::class,'createUser']);
