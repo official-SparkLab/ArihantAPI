7db5f47a8eb44e81c88550a9ebbaca4d8a5f8375
Prathamesh Varale <prathameshvarale07192@gmail.com>
2023-10-11 14:13:17 +0530

    Get import date

--- app/Http/Controllers/Post_Data_Controller.php
+++ app/Http/Controllers/Post_Data_Controller.php
@@ -4,29 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Post_Data_Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Post_Data_Controller extends Controller
 {
     //
-    public function addPostData(Request $request)
-    {
-        $postData = new Post_Data_Model;
-
-       $postData->Assignment = $request->input('Assignment');
-       $postData->Document_Type = $request->input('Document_Type');
-       $postData->Document_Date = $request->input('Document_Date');
-       $postData->Amount_Local_Currency = $request->input('Amount_Local_Currency');
-       $postData->Profit_Center = $request->input('Profit_Center');
-       $postData->Document_Number = $request->input('Document_Number');
-
-        $postData->save();
-
-        if($postData)
-        {
-            return response()->json(['message'=>'Payment added successfully']);
-        }
-        else{
-            return response()->json(['message'=>'Failed to add payment']);
-        }
-    }
+   public function getImportDate()
+   {
+    $data = DB::select("SELECT DISTINCT import_date from tbl_post_data;");
+    return response()->json(["data" => $data]);
+   }
 }
